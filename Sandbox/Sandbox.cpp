6f4c2d0ec411f640c54599719bb93ea2bcd4d92c
marcizhu <marc.izquierdo.hu@gmail.com>
2016-06-18 18:46:33 +0200

    First commit

---
+++ Sandbox/Sandbox.cpp
@@ -0,0 +1,61 @@
+// Sandbox.cpp : Defines the entry point for the console application.
+//
+
+#include <Windows.h>
+#include <vector>
+#include <stdio.h>
+
+#include "..\Cereal\Cereal.h"
+
+void gotoxy(int x, int y)
+{
+	CONSOLE_SCREEN_BUFFER_INFO SBInfo;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &SBInfo);
+
+	COORD dwPos;
+	x > 0 ? dwPos.X = x : dwPos.X = SBInfo.dwCursorPosition.X;
+	y > 0 ? dwPos.Y = y : dwPos.Y = SBInfo.dwCursorPosition.Y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), dwPos);
+}
+
+void setConsoleColor(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void dump(const void* object, unsigned int size, int color = 0x03)
+{
+	byte* x = (byte*)object;
+	std::vector<byte> data;
+
+	for (unsigned int i = 1; i <= size; i++)
+	{
+		x[i - 1] >= 32 ? x[i - 1] < 127 ? setConsoleColor(color) : setConsoleColor(0x07) : setConsoleColor(0x07);
+
+		printf("%02X ", x[i - 1]);
+		data.push_back(x[i - 1]);
+
+		if (((i % 16 == 0) && (i > 0)) || (i == size))
+		{
+			gotoxy(64, -1);
+
+			for (byte chars : data)
+			{
+				chars >= 32 ? chars < 127 ? setConsoleColor(color) : setConsoleColor(0x07) : setConsoleColor(0x07);
+				chars >= 32 ? chars < 127 ? printf("%c", chars) : printf(".") : printf(".");
+			}
+
+			data.clear();
+		}
+	}
+
+	printf("\n\n");
+	setConsoleColor(0x07);
+}
+
+int main()
+{
+	while (1) { _asm nop }
+
+    return 0;
+}
